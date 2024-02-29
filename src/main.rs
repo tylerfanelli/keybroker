e24be5e30171c18915dc1487e06843e5b55e1c53
Tyler Fanelli <tfanelli@redhat.com>
2024-02-29 16:26:46 -0500

    main: Start actix_web service with registration endpoint
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
 mod rats;
 
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| App::new().service(web::scope("/rvp").service(rats::rvp::register)))
+        .bind(("0.0.0.0", 8000))?
+        .run()
+        .await
 }
--- src/rats/mod.rs
+++ src/rats/mod.rs
@@ -1,3 +1,3 @@
 // SPDX-License-Identifier: Apache-2.0
 
-mod rvp;
+pub(super) mod rvp;
