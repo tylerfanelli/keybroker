95e4c9485e862c75dc9de6ca997ed458c0dba735
Tyler Fanelli <tfanelli@redhat.com>
2024-02-29 17:23:34 -0500

    rats/rvp: Keep reference count of RVP entries
    To avoid duplication of registered reference values, keep a reference
    count of all used entries. Calculate a SHA256 hash of reference values
    when they are submitted, and update the entry's reference count if an
    existing one is found. If an existing entry is not found, add a new
    entry into the RVP map.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/rats/rvp.rs
+++ src/rats/rvp.rs
@@ -4,26 +4,33 @@ use std::{collections::HashMap, sync::Mutex};
 
 use actix_web::{post, web::Json, HttpResponse};
 use lazy_static::lazy_static;
+use openssl::sha::Sha256;
 use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
+use serde_json::{to_string as json, Map, Value};
 use uuid::Uuid;
 
 /// A client's reference values to compare with its TEE evidence in the appraisal process.
 #[derive(Deserialize, Serialize)]
-pub(super) struct RvpRefValues {
+pub struct RvpRefValues {
     /// Queries on the TEE evidence with respect to the attestation policy.
-    query: Vec<String>,
+    pub query: Vec<String>,
 
     /// Reference TEE evidence information to compare with.
-    reference_values: Map<String, Value>,
+    pub reference_values: Map<String, Value>,
 
     /// Resources able to be fetched on a successful attestation.
-    resources: Map<String, Value>,
+    pub resources: Map<String, Value>,
 }
 
-// A map of guest UUIDs to reference values.
+#[derive(Default)]
+pub struct Rvp {
+    vals: HashMap<Uuid, (RvpRefValues, u8)>,
+    hashes: HashMap<[u8; 32], Uuid>,
+}
+
 lazy_static! {
-    pub(super) static ref RVP_MAP: Mutex<HashMap<Uuid, RvpRefValues>> = Mutex::new(HashMap::new());
+    // A map of guest UUIDs to reference values.
+    pub static ref RVP_MAP: Mutex<Rvp> = Mutex::new(Rvp::default());
 }
 
 // Lock the mutex and access the underlying RVP map.
@@ -34,15 +41,59 @@ macro_rules! rvp_map {
     };
 }
 
+impl Rvp {
+    /// Insert a set of reference values and return their associated UUID. Note that if the
+    /// reference values are already found, their reference count will be updated and the existing
+    /// UUID will be returned.
+    pub fn insert(&mut self, vals: RvpRefValues) -> Uuid {
+        let sha = self.hash_calc(&vals);
+
+        // Check if there is already an entry for this set of reference values.
+        if let Some(id) = self.hashes.get(&sha) {
+            let entry = self.vals.get_mut(id).unwrap();
+            (entry.1) += 1;
+
+            return *id;
+        }
+
+        // Create a new entry in both the value store and hashes store.
+        let id = Uuid::new_v4();
+        self.hashes.insert(sha, id);
+        self.vals.insert(id, (vals, 1));
+
+        id
+    }
+
+    /// Calculate a set of reference values' SHA256 hash.
+    pub fn hash_calc(&self, vals: &RvpRefValues) -> [u8; 32] {
+        let mut sha = Sha256::new();
+
+        for q in &vals.query {
+            sha.update(q.as_bytes());
+        }
+
+        for key in vals.reference_values.keys() {
+            sha.update(key.as_bytes());
+        }
+
+        for val in vals.reference_values.values() {
+            sha.update(json(val).unwrap().as_bytes());
+        }
+
+        for key in vals.resources.keys() {
+            sha.update(key.as_bytes());
+        }
+
+        for val in vals.resources.values() {
+            sha.update(json(val).unwrap().as_bytes());
+        }
+
+        sha.finish()
+    }
+}
+
 /// Register a client's reference values with the RATS reference value provider (RVP).
 #[post("/register")]
 pub async fn register(json: Json<RvpRefValues>) -> actix_web::Result<HttpResponse> {
-    let input: RvpRefValues = json.into_inner();
-
-    // Associate the guest's reference values with a UUID.
-    let id = Uuid::new_v4();
-
-    rvp_map!().insert(id, input);
-
-    Ok(HttpResponse::Ok().json(id.to_string()))
+    Ok(HttpResponse::Ok().json(rvp_map!().insert(json.into_inner()).to_string()))
 }
