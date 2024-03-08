a191b9c2b52a1233b40e640c95de3fe2d7f5fbaf
Tyler Fanelli <tfanelli@redhat.com>
2024-03-08 15:58:24 -0500

    rats/appraisal: Remove appraisal policy
    For simplicity, we can just examine the appraisal evidence without
    considering appraisal policies at the moment.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/rats/appraisal.rs
+++ src/rats/appraisal.rs
@@ -5,14 +5,12 @@ use anyhow::Result;
 #[allow(dead_code)]
 pub trait Appraiser {
     fn appraise(&self) -> Result<()> {
-        let policy = self.policy();
         let reference_values = self.reference_values();
         let input = self.input();
 
-        self.appraise_claims(policy, reference_values, input)
+        self.appraise_claims(reference_values, input)
     }
-    fn appraise_claims(&self, policy: String, reference: String, input: String) -> Result<()>;
-    fn policy(&self) -> String;
+    fn appraise_claims(&self, reference: String, input: String) -> Result<()>;
     fn reference_values(&self) -> String;
     fn input(&self) -> String;
 }
@@ -20,25 +18,11 @@ pub trait Appraiser {
 pub mod opa {
     use super::{super::rvp::RvpRefValues, *};
 
-    use std::sync::Mutex;
-
-    use lazy_static::lazy_static;
     use regorus::{Engine, QueryResults, Value};
     use serde::{Deserialize, Serialize};
 
-    lazy_static! {
-        pub static ref OPA_POLICY: Mutex<String> = Mutex::new("package policy".to_string());
-    }
-
-    macro_rules! opa_policy {
-        () => {
-            OPA_POLICY.lock().unwrap().clone()
-        };
-    }
-
     #[derive(Deserialize, Serialize)]
     pub struct OpaAppraiser {
-        pub policy: String,
         pub reference: String,
         pub input: String,
         pub queries: Vec<String>,
@@ -49,7 +33,6 @@ pub mod opa {
 
         fn try_from(data: (RvpRefValues, serde_json::Value)) -> Result<Self, Self::Error> {
             Ok(Self {
-                policy: opa_policy!(),
                 reference: serde_json::to_string(&data.0.reference_values).unwrap(),
                 input: serde_json::to_string(&data.1).unwrap(),
                 queries: data.0.query,
@@ -58,15 +41,12 @@ pub mod opa {
     }
 
     impl Appraiser for OpaAppraiser {
-        fn appraise_claims(&self, policy: String, reference: String, input: String) -> Result<()> {
+        fn appraise_claims(&self, reference: String, input: String) -> Result<()> {
             let mut engine = Engine::new();
 
             let reference = regorus::Value::from_json_str(&reference).unwrap();
             let input = regorus::Value::from_json_str(&input).unwrap();
 
-            engine
-                .add_policy("policy.rego".to_string(), policy)
-                .unwrap();
             engine.add_data(reference).unwrap();
             engine.set_input(input);
 
@@ -76,10 +56,6 @@ pub mod opa {
             }
 
             Ok(())
-        }
-
-        fn policy(&self) -> String {
-            self.policy.clone()
         }
 
         fn reference_values(&self) -> String {
