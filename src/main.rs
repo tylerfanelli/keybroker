a5393f64b838a958219ead5c9c0aa842f44d5ad7
Tyler Fanelli <tfanelli@redhat.com>
2024-03-29 00:03:20 -0400

    Specify IP address and port number on command line
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,24 @@ mod rats;
 use std::io;
 
 use actix_web::{web, App, HttpServer};
+use structopt::StructOpt;
+
+/// Command line arguments.
+#[derive(StructOpt)]
+struct Args {
+    /// IP address to run the server.
+    #[structopt(long)]
+    ip: String,
+
+    /// Port to run the server.
+    #[structopt(short, long)]
+    port: u16,
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    let args = Args::from_args();
+
     HttpServer::new(|| {
         App::new()
             .service(web::scope("/rvp").service(rats::rvp::register))
@@ -19,7 +34,7 @@ async fn main() -> io::Result<()> {
                     .service(kbs::resource),
             )
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind((args.ip, args.port))?
     .run()
     .await
 }
